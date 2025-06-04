77fc886f205f3e69f48d360e64f931536819bc91
Akampurira <akambenja1@gmail.com>
2025-06-04 16:12:28 +0300

    practice

---
+++ Basics.py
@@ -0,0 +1,22 @@
+# <- single line comment
+"""
+Multi line comment
+
+this is another commented line
+"""
+
+# print("this is my name") ctrl + /
+
+# python coding case (snake case)
+"""
+first_name
+benjamin_akampurira
+"""
+'''''
+
+
+'''
+
+# Indent
+first_name = "benjamin"
+print(first_name)
---
+++ variables.py
@@ -0,0 +1,27 @@
+## Assignment 1 Diffreciate between assigning and declaration
+
+## value
+
+## data type
+"""""
+integers
+floats
+strings/characters
+Boolean
+"""
+
+number_one = 30 # int
+number_two = 3.17 #float
+last_name = "Akampurira"
+first_name = "Benjamin"
+is_attentive =  False 
+is_present = True
+
+## checking for datatype
+print(type(number_two))
+
+##conversions
+new_number_two = int(number_two) 
+print(f"integer:{new_number_two} datatype: {type(number_two)}")
+
+age = "8" ## convert the variable age to integer 
