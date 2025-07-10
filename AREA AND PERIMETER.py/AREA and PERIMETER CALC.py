9bc6255d8158216240554e5429ea5d712efaad79
Akampurira <akambenja1@gmail.com>
2025-07-10 20:05:00 +0300

    Add files via upload

---
+++ AREA AND PERIMETER.py/AREA and PERIMETER CALC.py
@@ -0,0 +1,12 @@
+#ASK FOR THE LENGTH AND WIDTH
+length = float(input("Enter length"))
+width = float(input("Enter width"))
+
+#DEFINE AREA AND PERIMETER
+area = length * width
+perimeter = 2 * (length+width)
+
+#ASIGN THE OUTPUT
+print("AREA:", area)
+print("PERIMETER:", perimeter)
+
